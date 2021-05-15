1d757e7b9dd4cd6718a985ec050060a203c20dc1
Shane (Treasure, Luna) Xue <qq2198631773@outlook.com>
2021-05-15 17:31:07 +0800

    Flake8 was used to unify format

--- Random.py
+++ Random.py
@@ -7,12 +7,14 @@ from random import random
 
 SmallToBig = lambda a,b : (a,b) if a < b else (b,a)
 
+
 class RandGen():
-    def __init__(self , ceil, floor = 0 , randness = 3):
-        floor,ceil = SmallToBig(floor,ceil)
+    def __init__(self, ceil, floor=0, randness=3):
+        floor, ceil = SmallToBig(floor, ceil)
         self.range = ceil-floor+1
         self.floor = floor
-        #make sure the magnitude of randomness is AT LEAST 3 magnitudes bigger than the range
+        #make sure the magnitude of randomness is
+        #AT LEAST 3 magnitudes bigger than the range
         if randness > math.log(self.range,10) + 3:
             self.randness = randness
         else:
--- discrete_distributions.py
+++ discrete_distributions.py
@@ -1,26 +1,41 @@
 from random import random
 import numpy as np
 
+
 def bernoulli(successProb):
     return 1 if random() < successProb else 0
+
 
 def binomial(trials, successProb):
     """returns the times of trials that are successful in 'trials' trials"""
     return sum([bernoulli(successProb) for a in range(trials)])
+
 
 def negBinomial(fails_expect, successProb):
     fails = 0
     times = 0
     while fails != fails_expect:
         if bernoulli(successProb) == 0:
-            fails +=1
+            fails += 1
         times += 1
     return times
 
+
 def main():
-    print(np.mean([bernoulli(0.63) for a in range(10000)])) #sth around 0.63
-    print(np.mean([binomial(100, 0.63) for a in range(10000)])) #sth around 63
-    print(np.mean([negBinomial(37, 0.63) for a in range(10000)]))  # sth around 100
+    print(np.mean([bernoulli(0.63)
+                  for a in range(10000)]))  # sth around 0.63
+    print(np.mean([binomial(100, 0.63)
+                  for a in range(10000)]))  # sth around 63
+    print(np.mean([negBinomial(37, 0.63)
+                  for a in range(10000)]))   # sth around 100
+
+
+def test_bernoulli():
+    for a in range(10000):
+        rand = random()
+        assert abs(np.mean([bernoulli(rand)
+                   for a in range(10000)]) - rand) < 0.1
+
 
 if __name__ == "__main__":
     main()
